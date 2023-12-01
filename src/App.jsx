dc054076482c26eb5284043835732821829467e9
Andrew Wyatt <aewyatt7@gmail.com>
2023-12-01 09:29:15 -0700

    reactintro

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  //Destructuring
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1> React Intro Exercise</h1>
+    <p>This is how we get to ESPN</p>
+    <button href= "https://www.espn.com"</button>
+    
+    
+    </>
+  )
+}
+
+export default App
